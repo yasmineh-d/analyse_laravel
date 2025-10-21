6507606b4e83e8f21d59d134a6473f23ea8a5b8f
yasmineh-d <yassminehadad75@gmail.com>
2025-10-21 12:29:13 +0100

    Remove Mini_site_Portfolio_developpeur from repository

---
+++ Mini_site_Portfolio_developpeur/Portfolio/resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Developper</title>
+    
+</head>
+<body>
+    <!-- le site a trois things principale 1.header   2.main      3.footer -->
+    <main>
+        @yield('content')
+    </main>
+    
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</body>
+</html>
